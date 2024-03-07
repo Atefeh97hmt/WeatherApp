367e80b3e65e73f901ab21f2f537e26270177744
atefeh <hemmati.atefeh97@gmail.com>
2024-03-07 22:31:26 +0330

    navigation component created and used

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import './assets/main.css'
-
+import './assets/output.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwind.css'
 
 const app = createApp(App)
 
