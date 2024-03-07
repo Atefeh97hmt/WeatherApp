003eef6295c928dd0a7fadc3b0ba810479af78f0
atefeh <hemmati.atefeh97@gmail.com>
2024-03-07 22:02:09 +0330

    tailwind theme extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,vue,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+colors:{
+  "weather-primary":"#00668A",
+  "weather-secondary":"#004E71",
+
+},
+
+container:{
+  
+  padding:"2rem",
+  center:"true"
+},
+
+screens:{
+  sm:"640px",
+  md:"768px"
+}
+
+    },
   },
   plugins: [],
 }
